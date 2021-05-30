88289cb551fffa42b1cd3eaa66c9c9e5a312a2b8
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-05-30 16:43:56 +0530

    register pop up ui created

--- Data Entry/Register/RegisterViewController.swift
+++ Data Entry/Register/RegisterViewController.swift
@@ -17,8 +17,37 @@ class RegisterViewController: UIViewController {
     private var viewModel: RegisterViewModel!
     
     // MARK: IBOutlets
-
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var registerLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+    }
+    
+    // MARK: IBActions
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        print("submit")
+    }
+}
+
+// MARK: - Private Methods
+private extension RegisterViewController {
+    
+    func setupViews() {
+        navigationController?.isNavigationBarHidden = true
+        let radius = CGFloat(10)
+        containerView.layer.cornerRadius = radius
+        emailTextField.layer.cornerRadius = radius
+        firstNameTextField.layer.cornerRadius = radius
+        lastNameTextField.layer.cornerRadius = radius
+        phoneNumberTextField.layer.cornerRadius = radius
+        submitButton.layer.cornerRadius = radius
     }
 }
