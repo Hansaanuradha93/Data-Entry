9576e32968dedde16d47d3ca2d52483fb829a428
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-05-30 19:59:26 +0530

    detail cell added

---
+++ Data Entry/Cells/DetailCell/DetailCell.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class DetailCell: UITableViewCell {
+
+    // MARK: IBOutlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+
+// MARK: - Methods
+extension DetailCell {
+    
+    func set(user: User) {
+        nameLabel.text = "\(user.firstName ?? "") \(user.lastName ?? "")"
+        emailLabel.text = "\(user.email ?? "")"
+        phoneNumberLabel.text = "\(user.phoneNumber ?? "")"
+    }
+}
