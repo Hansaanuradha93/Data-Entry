34307d0a1360b1e80314ed970af0dec718cc73da
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-05-30 20:24:59 +0530

    extensions added

--- Data Entry/Utilities/Constants.swift
+++ Data Entry/Utilities/Constants.swift
@@ -1,5 +1,16 @@
-import Foundation
+import UIKit
 
 struct Strings {
     static let youHaveSuccessfullyFavouritedTheUser = "You have successfully favourited this user 🎉"
+}
+
+
+// MARK: - Fonts
+struct Fonts {
+    static let avenirNext = "Avenir Next"
+}
+
+// MARK: - Asserts
+struct Asserts {
+    static let docFill = UIImage(systemName: "doc.text.fill")!
 }
